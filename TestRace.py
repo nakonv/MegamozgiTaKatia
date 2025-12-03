50702a6e5798d2f6d9a3eb5b144bf5256d3707be
nakonv <nakonechna.viktoriia@lll.kpi.ua>
2025-12-03 23:54:26 +0200

    Refactor TestRace.py with mock classes and new tests
    Refactor tests to use mock classes for track and car. Added a new test for simulating a lap.

--- TestRace.py
+++ TestRace.py
@@ -1,22 +1,54 @@
 import unittest
-from track import Track
 from driver import Driver
-from Car import Car
 from Race import Race
 
+class MockTrack:
+    def __init__(self, length):
+        self.length = length
+
+    def getSectionSpeedFactor(self, lapIndex):
+        return 1.0
+
+class MockCar:
+    def __init__(self, speed):
+        self.speed = speed
+        self.wear_updates = 0
+
+    def calculate_speed(self, factor):
+        return self.speed * factor
+
+    def update_wear(self):
+        self.wear_updates += 1
 
 class MyTestCase(unittest.TestCase):
     def test_addParticipant(self):
-        track = Track("TestTrack", 3000, 10, grip=0.8)
-        race = Race(track, laps=3)
+        track = MockTrack(5000)
+        race = Race(track, laps=1)
         driver = Driver(name="Tester", aggression=0.3, skill=0.8, mistakeChance=0.1, overtakingRisk=0.2)
-        car = Car(team_name="Ferrari", engine_power=800, aerodynamics=0.9, tire_grip=0.85, fuel=100, tire_wear=0, speed=250.0)
+        car = MockCar(250)
 
         race.addParticipant(driver, car)
 
         self.assertEqual(len(race.drivers), 1)
         self.assertEqual(len(race.cars), 1)
 
+    def test_simulateLap(self):
+        track = MockTrack(5000)
+        race = Race(track, laps=1)
+        driver = Driver(name="Tester", aggression=0.3, skill=0.8, mistakeChance=0.1, overtakingRisk=0.2)
+        car = MockCar(250)
+
+        race.addParticipant(driver, car)
+        race.startRace()
+
+        result_names = [r.driver.name for r in race.results]
+        self.assertIn("Tester", result_names)
+
+        expected_time = 5000 / 250  # length / speed
+        total_time = race.results[0].total_time
+        self.assertAlmostEqual(total_time, expected_time, places=4)
+
 
 if __name__ == '__main__':
     unittest.main()
+
