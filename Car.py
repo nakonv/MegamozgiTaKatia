ed422df58df0702004c3e959d6ddfa98251b8be9
tryfanovakateryna <tryfanova.kateryna@lll.kpi.ua>
2025-12-05 15:18:10 +0200

    Delete Fleet class and related methods
    Removed Fleet class and its associated methods.

--- Car.py
+++ Car.py
@@ -45,35 +45,3 @@ class Car:
         self.fuel = max(0.0, self.fuel - fuel_consumption)
         self.tire_wear = min(100.0, self.tire_wear + tire_degradation)
         print(f"Оновлено: пальне = {self.fuel:.1f}%, зношеність шин = {self.tire_wear:.1f}%")
-
-class Fleet:
-    def __init__(self, name):
-        self.name = name
-        self.cars = [] 
-
-    def add_car(self, car):
-        self.cars.append(car)
-        print(f"Авто '{car.team_name}' додано до автопарку '{self.name}'")
-
-    def remove_car(self, car):
-        if car in self.cars:
-            self.cars.remove(car)
-            print(f"Авто '{car.team_name}' видалено з автопарку")
-        else:
-            print("Авто не знайдено в автопарку.")
-
-    def print_fleet(self):
-        print(f"\nАвтопарк: {self.name}")
-        print("Машини у складі:")
-        if not self.cars:
-            print("  (порожньо)")
-            return
-        for car in self.cars:
-            car.print_specs()
-
-    def service_all(self):
-        print(f"\nПроводимо обслуговування всіх авто в автопарку '{self.name}':")
-        for car in self.cars:
-            car.refuel(100)
-            car.change_tires(car.tire_grip)
-
