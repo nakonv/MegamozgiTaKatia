63b66838d3f218cc465fddce0e9bd70c63275633
nakonv <nakonechna.viktoriia@lll.kpi.ua>
2025-12-05 12:18:45 +0200

    Enhance RaceManage with race management features
    Added methods to manage races and count unique drivers.

--- RaceManager.py
+++ RaceManager.py
@@ -1,6 +1,18 @@
+from Race import Race
+
 class RaceManage:
     def __init__(self, name):
         self.name = name
         self.races = []  
+        
+    def addRace(self, race: Race):
+        self.races.append(race)
 
+    def totalDrivers(self):
+        drivers = set()
+        for race in self.races:
+            for d in race.drivers:
+                drivers.add(d.name)
+        return len(drivers)
   
+
