a8fc2d7e8d7d20bc58d10b94c7190c52216dbd4e
nakonv <nakonechna.viktoriia@lll.kpi.ua>
2025-12-19 14:54:38 +0200

    Replace getSectionSpeedFactor with trackModifier
    Refactored track speed factor methods to use trackModifier.

--- track_types.py
+++ track_types.py
@@ -1,16 +1,13 @@
 from track import Track
 
 class CityTrack(Track):
-  def getSectionSpeedFactor(self, sectionIndex):
-    base = super().getSectionSpeedFactor(sectionIndex)
-    return base * 0.85
+  def trackModifier(self):
+    return 0.85
 
 class RaceTrack(Track):
-  def getSectionSpeedFactor(self, sectionIndex):
-    base = super().getSectionSpeedFactor(sectionIndex)
-    return base * 1.1
+  def trackModifier(self):
+    return 1.1
 
 class WetTrack(Track):
-  def getSectionSpeedFactor(self, sectionIndex):
-    base = super().getSectionSpeedFactor(sectionIndex)
-    return base * (0.7 + self.grip * 0.3)
+  def trackModifier(self):
+    return 0.7 + self.grip * 0.3
