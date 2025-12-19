2c6d4b3fffdf7d773fe3ef3cbc29e791e391edf0
nakonv <nakonechna.viktoriia@lll.kpi.ua>
2025-12-19 15:09:31 +0200

    Implement TrackFactory for track creation
    Added TrackFactory class to create different track types.

--- track_types.py
+++ track_types.py
@@ -1,5 +1,17 @@
 from track import Track
 
+class TrackFactory:
+  @staticmethod
+  def create(track_type, name, length, turns, grip):
+    if track_type == "city":
+      return CityTrack(name, length, turns, 0.0, grip)
+    elif track_type == "race":
+      return RaceTrack(name, length, turns, 0.0, grip)
+    elif track_type == "wet":
+      return WetTrack(name, length, turns, 0.0, grip)
+    else:
+      raise ValueError("Невідомий тип треку")
+
 class CityTrack(Track):
   def trackModifier(self):
     return 0.85
