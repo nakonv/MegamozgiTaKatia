b3f2847e851404a0eca15cf2c3063757250e4c4e
nakonv <nakonechna.viktoriia@lll.kpi.ua>
2025-12-19 12:54:40 +0200

    Add main function for race management
    Implement main function to manage race and championship results.

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-# from track_types import 
-# from race import Race
+from track_types import CityTrack, RaceTrack, WetTrack
+from race import Race
 
+def main():
+  track = WetTrack("Spa", 7004, 20, grip=0.7)
+  race = Race(track, laps=3)
+
+  #додати драйверів
+  #додати машинки
+
+  race.addParticipant(driver1, car1)
+  race.addParticipant(driver2, car2)
+
+  race.startRace()
+  race.saveResults("results.txt")
+
+  championship = RaceManage("F1 Demo")
+  championship.addRace(race)
+  championship.saveChampionshipResults("championship.txt")
+
+  print("Гонка завершена. Результати збережені.")
+
+if __name__ == "__main__":
+    main()
